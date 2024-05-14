b33f83da2cd247662aea4d1debe310bd7300b244
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-05-14 17:27:35 -0300

    ph

---
+++ 01-hello-world.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start php</title>
+</head>
+<body>
+    <h1>Exp PHP</h1>
+    <hr>
+
+    <!-- Delimitadores de abertura/fechamento -->
+    <?php
+    
+    echo "ola"; 
+    echo "<p>Senac Penha</p>"
+    echo '<p> style="text-align:center">Senac Penha</p>'
+    echo "<p> style='text-align:center'>Senac Penha</p>"
+    echo "<p> style=\"text-align:center\">Senac Penha</p>"
+
+    /* Delimitadores de abertura/fechamento */
+    ?>
+</body>
+</html>
