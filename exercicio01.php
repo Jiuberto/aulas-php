979675e85cfcace4ea3925793372b2d18af076ed
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-05-15 15:30:14 -0300

    vs

--- exercicio01.php
+++ exercicio01.php
@@ -4,18 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .um {color: red;}
+        .dois {color: purple;}
+    </style>
 </head>
+
 <body>
+    <div class="conteiner">
     <h1>Exercicio 1 de php</h1>
     <hr>
 
     <?php
 
     $nome = "Aragorn";
-    const IDADE = "80"
+    $ano = "86";
+    const IDADE = "80";
 
-    echo "<p> Meu nome é $nome e tenho " .IDADE. " anos</p>"
+    echo "<p> Meu nome é $nome e tenho " .IDADE. " anos.</p>"
 
     ?>
+
+    <p> Meu nome é <b class="um"><?=$nome?></b> e tenho <i class="dois"><?=$ano?></i> anos. </p>
+
+    
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
