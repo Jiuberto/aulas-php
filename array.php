f6153f857a5905725ef1075bcc3c6e5437c0daaf
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-05-15 16:32:59 -0300

    arr

--- array.php
+++ array.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
+    <div>
     <h1>array</h1>
     <hr>
 
@@ -14,10 +15,39 @@
     $bandas = ["Rush", "Slayer", "Nightwish"];
     $cursos = array("HTML5", "PHP", "Design", "JS");
 
-    define("UBIDADES_SESC", ["Belenzinho", "Bertioga"]);
+    define("UNIDADES_SESC", ["Belenzinho", "Bertioga"]);
     const UNIDADE_SESAC = ["Penha", "São Miguel", "Itaquera"];
 
     ?>
-    
+    <h3>Acesso e saída de dados</h3>
+    <p>Curto a banda <?=$bandas[1]?>.</p>
+    <p>Já estudei <?=$cursos[3]?></p>
+    <p>Gostaria de conhecer o Sesc <?=UNIDADES_SESC[1]?></p>
+    <p>Estamos no Senac <?=UNIDADE_SESAC[0]?></p>
+
+    <hr>
+
+    <h2>Arrays associados</h2>
+    <?php
+    $filme = [
+        //Chave associativa
+        "titulo" => "Principe do Egito", 
+        "ano" => 1998,
+        "genero" => "Ação",
+        "personagens" => ["Moisés","Ramsés"]
+
+    ];
+
+    $livro = array(
+        "titulo" => "Senhor dos Anéis",
+        "autor" => "Tolkien"
+
+    );
+    ?>
+    <h3>Acesso e saia de dados</h3>
+    <p>O filme <?=$filme["titulo"]?> foi lançado em <?=$filme["ano"]?> </p>
+    <p> <?=$livro['titulo']?> é um ótimo livro</p>
+
+    </div>
 </body>
 </html>
