35e706de2837b2ecdea9296d0ae0eab06de0af6b
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-05-21 09:36:25 -0300

    cor

--- exercicios03.php
+++ exercicios03.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .even {
+            background-color: #f0f8ff; /* AliceBlue */
+        }
+        .odd {
+            background-color: #faebd7; /* AntiqueWhite */
+        }
+    </style>
 </head>
 <body>
 
@@ -12,16 +20,15 @@
 
     <?php
     $meses = ["Janeiro","Fevereiro","Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro","Outubro", "Novembro","Dezembro"];
-    
     ?>
 
     <ol class="alert alert-dark">
         <?php 
-        $numero = 1;
-        foreach ($meses as $mes) { ?>
-            <li><?=$mes?></li>
+        foreach ($meses as $index => $mes) { 
+            $class = ($index % 2 == 0) ? 'even' : 'odd';
+        ?>
+            <li class="<?= $class ?>"><?= $mes ?></li>
         <?php 
-        $numero++;
         } ?>
     </ol>
 
