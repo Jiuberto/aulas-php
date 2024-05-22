df0d37c8e2ffbe6e0de4d999ad4782f8214ecb19
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-05-22 15:25:36 -0300

    8

---
+++ 08-formulario-com-processamento.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário com PHP</title>
+</head>
+
+<body>
+    <h1>Formulário e processamento juntos</h1>
+    <hr>
+
+    <?PHP
+    /* Detectando o envio/acionamento do formulário */
+    if ( isset($_POST['enviar']) ) {
+        $nome = $_POST["nome"];
+
+?>
+
+        <h2>Bem-vindo(a) <?=$nome?>!</h2>
+        <hr>
+<?php
+} else {
+    /* O formulario abaixo sera exibido enquanto o botão enviar não for acionado. Qundo for, o formulario será removido da página */
+?>
+
+
+    <!-- Mantenha o action vazio -->
+    <form autocomplete="off" action="" method="post"> <label for="nome">Nome:</label> <input type="text" name="nome" id="nome"> <button type="submit" name="enviar">Enviar</button> </form>
+
+    <?php } ?>
+</body>
+
+</html>
